2a33d8f7f373576d86d377e172ccb6a84adff2f6
ezbueno <ezandrobueno@yahoo.com.br>
2021-07-01 17:28:01 -0300

    Developed Exception Handler

---
+++ src/main/java/com/buenoezandro/bookstore/exception/ApiError.java
@@ -0,0 +1,28 @@
+package com.buenoezandro.bookstore.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError {
+
+	private int code;
+	private String status;
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyy hh:mm:ss")
+	private LocalDateTime timestamp;
+
+	private String message;
+	private List<String> errors;
+
+}
