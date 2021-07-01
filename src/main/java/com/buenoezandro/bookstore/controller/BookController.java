0209b469a43aa3b484d01925057d53ba711bf9ea
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-30 21:41:28 -0300

    Config project

--- src/main/java/com/buenoezandro/bookstore/controller/BookController.java
+++ src/main/java/com/buenoezandro/bookstore/controller/BookController.java
@@ -4,10 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 @RequestMapping(value = "/api/v1/books")
 public class BookController {
-	
+
+	@ApiOperation(value = "Hello, Bookstore Manager!")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Success method return") })
 	@GetMapping
 	public String hello() {
 		return "Hello, Bookstore Manager!";
---
+++ src/main/java/com/buenoezandro/bookstore/custominfoendpoint/InfoWebPointExtension.java
@@ -0,0 +1,38 @@
+package com.buenoezandro.bookstore.custominfoendpoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointResponse;
+import org.springframework.boot.actuate.endpoint.web.annotation.EndpointWebExtension;
+import org.springframework.boot.actuate.info.InfoEndpoint;
+import org.springframework.stereotype.Component;
+
+@Component
+@EndpointWebExtension(endpoint = InfoEndpoint.class)
+public class InfoWebPointExtension {
+	
+	private InfoEndpoint delegate;
+	private static final Integer STATUS = 200;
+	
+	@Autowired
+	public InfoWebPointExtension(InfoEndpoint delegate) {
+		this.delegate = delegate;
+	}
+	
+	@ReadOperation
+	public WebEndpointResponse<Map<String, Object>> info() {
+		Map<String, Object> info = this.delegate.info();
+		Integer status = this.getStatus(info);
+		Map<String, Object> customInfo = new HashMap<>(info);
+		customInfo.put("customInfo", "handOn");
+		return new WebEndpointResponse<>(customInfo, status);
+	}
+
+	private Integer getStatus(Map<String, Object> info) {
+		return STATUS;
+	}
+
+}
