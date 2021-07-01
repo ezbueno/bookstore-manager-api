f2a81e1155d98df92cf4a440ca1d79758548361b
ezbueno <ezandrobueno@yahoo.com.br>
2021-07-01 18:11:03 -0300

    Created Author DTO class

---
+++ src/main/java/com/buenoezandro/bookstore/author/dto/AuthorDTO.java
@@ -0,0 +1,28 @@
+package com.buenoezandro.bookstore.author.dto;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorDTO {
+
+	private Long id;
+
+	@NotNull
+	@NotEmpty
+	@Size(max = 255)
+	private String name;
+
+	@NotNull
+	@Max(value = 120)
+	private int age;
+
+}
