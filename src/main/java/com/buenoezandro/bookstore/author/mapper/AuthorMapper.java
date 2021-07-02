bc0191d08f3ca293dd8d2498955888bac2fa59a1
ezbueno <ezandrobueno@yahoo.com.br>
2021-07-01 21:37:31 -0300

    Updated Author Mapper

--- src/main/java/com/buenoezandro/bookstore/author/mapper/AuthorMapper.java
+++ src/main/java/com/buenoezandro/bookstore/author/mapper/AuthorMapper.java
@@ -1,6 +1,7 @@
 package com.buenoezandro.bookstore.author.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import com.buenoezandro.bookstore.author.dto.AuthorDTO;
@@ -11,6 +12,9 @@ public interface AuthorMapper {
 
 	AuthorMapper INSTANCE = Mappers.getMapper(AuthorMapper.class);
 
+	@Mapping(target = "createdDate", ignore = true)
+	@Mapping(target = "lastModifiedDate", ignore = true)
+	@Mapping(target = "books", ignore = true)
 	Author toModel(AuthorDTO authorDTO);
 
 	AuthorDTO toDTO(Author author);
