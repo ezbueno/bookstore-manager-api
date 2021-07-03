232985d22fdd1e527ba3516ea447077da6bdfdc4
ezbueno <ezandrobueno@yahoo.com.br>
2021-07-03 15:35:51 -0300

    Created Author Repository

--- src/main/java/com/buenoezandro/bookstore/author/repository/AuthorRepository.java
+++ src/main/java/com/buenoezandro/bookstore/author/repository/AuthorRepository.java
@@ -1,5 +1,7 @@
 package com.buenoezandro.bookstore.author.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,5 @@ import com.buenoezandro.bookstore.author.entity.Author;
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+	Optional<Author> findByName(String name);
 }
