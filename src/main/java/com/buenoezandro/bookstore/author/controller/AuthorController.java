edc5a58a1f5739a48b251e0b4d45219801859e76
ezbueno <ezandrobueno@yahoo.com.br>
2021-07-03 15:34:26 -0300

    Created Author Controller

--- src/main/java/com/buenoezandro/bookstore/author/controller/AuthorController.java
+++ src/main/java/com/buenoezandro/bookstore/author/controller/AuthorController.java
@@ -1,9 +1,16 @@
 package com.buenoezandro.bookstore.author.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.buenoezandro.bookstore.author.dto.AuthorDTO;
 import com.buenoezandro.bookstore.author.service.AuthorService;
 
 @RestController
@@ -15,6 +22,12 @@ public class AuthorController implements AuthorControllerDocs {
 	@Autowired
 	public AuthorController(AuthorService authorService) {
 		this.authorService = authorService;
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public AuthorDTO create(@Valid @RequestBody AuthorDTO authorDTO) {
+		return authorService.create(authorDTO);
 	}
 
 }
