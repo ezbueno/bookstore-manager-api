ca5990a4d18ccc4badeb299994d914b285c90627
ezbueno <ezandrobueno@yahoo.com.br>
2021-07-07 18:49:18 -0300

    Created Publisher DTO

---
+++ src/main/java/com/buenoezandro/bookstore/publisher/dto/PublisherDTO.java
@@ -0,0 +1,34 @@
+package com.buenoezandro.bookstore.publisher.dto;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PublisherDTO {
+
+	private Long id;
+
+	@NotEmpty
+	@Size(max = 255)
+	private String name;
+
+	@NotEmpty
+	@Size(max = 255)
+	private String code;
+
+	@NotNull
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+	private LocalDateTime foundationDate;
+
+}
